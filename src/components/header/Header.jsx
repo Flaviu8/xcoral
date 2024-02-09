6d4ab1612877a79208b0e6ac81df1db038507378
Flaviu10 <flavius_flaviu93@yahoo.com>
2024-02-09 22:12:14 +0000

    add max width

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,15 @@
 import banner from "../../assets/header.mp4"
+import Search from "../search/Search"
 import './header.css'
 
 export default function Header() {
   return (
     <div className="banner">
-      <div className="text-absolute">
-        <h1> Vrei sa fii sigur ca masina pe care o cumperi nu a fost furata sau avariata? Atunci alege site-ul nostru si cumpara doar masini verificate.</h1>
-      </div>
         <video src={banner} autoPlay playsInline muted loop width="100%" height="auto"></video>
+        <div className="text-absolute">
+        <h1>Vrei sa ai o masina fara avarii sau cu probleme la kilometri? Atunci alege site-ul nostru cu masini verificate.</h1>
+      </div>
+        <Search /> 
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,11 @@
 import Cards from "../../components/cards/Cards";
 import Header from "../../components/header/Header";
-import Search from "../../components/search/Search";
 
 
 export default function Home() {
   return (
     <div>
         <Header />
-        <Search />
         <Cards />
     </div>
   )
